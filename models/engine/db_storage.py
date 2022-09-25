6ee1f54058a7ca846bb4cdd1398ab6650e23ad40
belovetech <belovetech@gmail.com>
2022-09-25 12:58:12 +0100

    [Update]: query current database session all objects

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,9 +5,21 @@ from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models.base_model import Base
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
 
 
 class DBStorage:
+    """Represent database storage
+
+    Attributes:
+        __engine (sqlalchemy.Engine): SQlAlchemy database engine
+        __session (sqlalchemy.Session): SQlAlchemy database session
+    """
     __engine = None
     __session = None
 
@@ -18,15 +30,36 @@ class DBStorage:
             'mysql+mysqldb://{}:{}@{}/{}'.format(
                 getenv('HBNB_MYSQL_USER'),
                 getenv('HBNB_MYSQL_PWD'),
+                getenv('HBNB_MYSQL_HOST'),
                 getenv('HBNB_MYSQL_DB'),
                 pool_pre_ping=True))
-
-        Session = sessionmaker()
-        self.__session = Session()
 
         if getenv('HBNB_ENV') == 'test':
             # Drop all the tables
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """"""
+        """query on the current database session all objects depending of the class name
+
+        if cls=None:
+             query all types of objects (User, State, City, Amenity, Place and Review)
+
+        Returns:
+            a dictionary: key = <class-name>.<object-id>
+        """
+        dict = {}
+        if cls and type(cls) == 'str':
+            cls = eval(cls)
+            query = self.__session.query(cls)
+            for q in query:
+                key = type(q).__name__ + '.' + q.id
+                dict[key] = q
+        else:
+            classes = [User, State, City, Amenity, Place, Review]
+            for cls in classes:
+                query = self.__session.query(cls)
+                for q in query:
+                    key = type(q).__name__ + '.' + q.id
+                    dict[key] = q
+                # return {'{}.{}'.format(type(q).__name__, q.id): q for q in query}
+        return dict
