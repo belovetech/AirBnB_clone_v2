9fcf70ed179129f66d2964ae20ac6564101f7680
belovetech <belovetech@gmail.com>
2022-09-26 08:38:03 +0100

    [Feat]: add city object getter

--- models/state.py
+++ models/state.py
@@ -2,8 +2,11 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 from models.base_model import Base
+from models.city import City
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+from os import getenv
+import models
 
 
 class State(BaseModel, Base):
@@ -17,4 +20,14 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship('City', backref='states',
-                          cascade='all delete delete-orphan')
+                          cascade='all, delete, delete-orphan')
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """Get a list of all related City objects"""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
