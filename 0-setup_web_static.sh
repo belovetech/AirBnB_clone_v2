e0fc1acfcf73d7b7d4480d4db64eb46e4b4842c0
belovetech <belovetech@gmail.com>
2022-09-29 18:05:58 +0100

    [Update]: prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
 
+# Install Nginx
 apt-get install -y nginx
 
+
+# Create web_static path
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared
 touch /data/web_static/releases/test/index.html
 
+# Print 'Hello Nginx'
+echo "Hello Nginx!" >> /data/web_static/releases/test/index.html
+
+# Create a symbolink link
 ln -s -f /data/web_static/releases/test/ /data/web_static/current
-chown -R $USER:$USER /data
 
-printf %s "{
-		
-	location /hbnb_static {
-		alias /
-	}
+# Give ownership to the ubuntu
+chown -hR "ubuntu":"ubuntu" /data
 
-}" >> /ete/nginx/sites-available/default
+# Update Nginx configuration
+sed -i "38i\\\tlocation /hbnb_static/{\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+
+# Restart Nginx
+service nginx restart
 
