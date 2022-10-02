669a701e351aef67c7c0f4fb7d24bd988cf97f08
belovetech <belovetech@gmail.com>
2022-10-02 23:29:15 +0100

    [Update]: use try and except block

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,6 +4,7 @@ using created function deploy and pack"""
 
 from fabric.api import *
 import os
+
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
@@ -12,8 +13,9 @@ env.hosts = ['35.153.143.199', '44.210.236.124']
 
 def deploy():
     """Pack and deploy all files"""
-    archive_path = do_pack()
-    if not archive_path:
+    try:
+        archive_path = do_pack()
+    except Exception:
         return False
 
     return do_deploy(archive_path)
