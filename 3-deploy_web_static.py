c7997b2363db79cd8e2039015dfb6148dbcefcd2
belovetech <belovetech@gmail.com>
2022-10-02 23:16:42 +0100

    [Feat]: Pack and deploy all files to server

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Creates and distributes an archive to web servers,
+using created function deploy and pack"""
+
+from fabric.api import *
+import os
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
+
+env.hosts = ['35.153.143.199', '44.210.236.124']
+
+def deploy():
+    """Pack and deploy all files"""
+    archive_path = do_pack()
+    if not archive_path:
+        return False
+    
+    return do_deploy(archive_path)
