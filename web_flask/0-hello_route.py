433578e140c054cc5fd9b5447daebbb841b9ee45
belovetech <belovetech@gmail.com>
2022-10-14 16:04:37 +0100

    [Update]: add comment to the route function

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,9 +8,10 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Function invoked with / route"""
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
