5f4d7be9ef6c2fb8cba78871d185e252bb9d3334
belovetech <belovetech@gmail.com>
2022-10-15 12:39:26 +0100

    [Feat]: defines users tables

--- models/user.py
+++ models/user.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+import email
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """Representation of User class
+    args:
+        __tablename___(str): Name of the table
+        name (String): name of users
+        password (String): password of users
+        first_name (String): first_name of users
+        last_name (String): last_name of users
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
