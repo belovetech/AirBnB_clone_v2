908c7533e3b6ad646412551d12f5fd3d2bbf1b51
belovetech <belovetech@gmail.com>
2022-10-16 10:29:03 +0100

    [Update]: fix id not found in kwargs

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,7 @@
 '''The Package initializer'''
 from os import getenv
 
-if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
+if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
--- models/base_model.py
+++ models/base_model.py
@@ -17,9 +17,10 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        self.id = str(uuid.uuid4())
         if not kwargs:
             from models import storage
-            self.id = str(uuid.uuid4())
+            
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
@@ -28,7 +29,7 @@ class BaseModel:
                     kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
             else:
                 self.created_at = datetime.now()
-            if kwargs.get("created_at"):
+            if kwargs.get("updated_at"):
                 kwargs["updated_at"] = datetime.strptime(
                     kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
             else:
@@ -60,16 +61,8 @@ class BaseModel:
         if '_sa_instance_state' in dictionary.keys():
             del dictionary['_sa_instance_state']
         
-        print(dictionary)
         return dictionary
-        
-    #     my_dict = dict(self.__dict__)
-    #     my_dict["__class__"] = str(type(self).__name__)
-    #     my_dict["created_at"] = self.created_at.isoformat()
-    #     my_dict["updated_at"] = self.updated_at.isoformat()
-    #     if '_sa_instance_state' in my_dict.keys():
-    #         del my_dict['_sa_instance_state']
-    #     return my_dict
+
 
     def delete(self):
         """delete the current instance from storage"""
