7161dd072fe580b8fa0a903eb1008a1d89c84b12
belovetech <belovetech@gmail.com>
2022-10-17 00:05:11 +0100

    [Feat]: add static files to web_flask

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Script that runs a Flask web application"""
+
+import os
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    storage.close()
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def state_city(id=None):
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
+    # states = storage.all("State")
+    # for state in states.values():
+    #     if state.id == id:
+    #         return render_template('10-hbnb_filters.html',
+    #                                Table=state.name,  state=state)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
