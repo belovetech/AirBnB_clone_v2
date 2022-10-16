0e05f319f8a14c28bb7a63205be391cc727ad9b1
belovetech <belovetech@gmail.com>
2022-10-16 22:39:58 +0100

    [Feat]: List city by state id

---
+++ web_flask/9-states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Script that runs a Flask web application"""
+
+import os
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """List all states"""
+    states = storage.all("State")
+    return render_template('7-states_list.html',
+                           Table="States", states=states)
+               
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_city(id=None):
+    states = storage.all("State")
+    
+    for state in states.value():
+        if states.id == id:
+            return render_template('9-states.html', Table=state.name,  state=state)
+    return render_template('9-states.html', Table=state.name,  state=None)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
